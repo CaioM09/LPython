beb80f0aec869806fa2b8770bfc4ec95a2f4baa7
Caio M <caio.3151@gmail.com>
2021-03-09 12:45:28 -0300

    Adicionando o capitulo 2

---
+++ cap2/programas/programa2-1.py
@@ -0,0 +1,6 @@
+# Programa 2.1 - Primeiro programa com variáveis
+
+a = 2
+b = 3
+
+print(a + b)
---
+++ cap2/programas/programa2-3.py
@@ -0,0 +1,3 @@
+# Programa 2.3 - Alternativa para o cálculoo de aumento de salário
+
+print(1500 + (1500 * 5 / 100))
